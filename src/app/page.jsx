7c3df672e6d383315269b97bc91fdc7d222b4860
Jaimin Patel <jaiminm.patel@mail.utoronto.ca>
2025-05-07 17:17:22 -0400

    changed the logo

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -38,7 +38,9 @@ function NavBar() {
     <nav className="bg-white shadow-md fixed top-0 left-0 right-0 z-50">
       <div className="container mx-auto px-4 py-3">
         <div className="flex justify-between items-center">
-          <a href="#" className="text-blue-600 text-2xl font-bold">&lt;JP/&gt;</a>
+          <a href="#" className="text-blue-600 text-2xl font-bold w-10 h-10 flex items-center justify-center hover:text-white transition duration-300 transform hover:-translate-y-1">
+            <img src="favicon.ico" alt="Logo"/>
+          </a>
           
           {/* Mobile menu button */}
           <div className="md:hidden">
